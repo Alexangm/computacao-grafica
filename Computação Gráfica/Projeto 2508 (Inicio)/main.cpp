5a0d572f63a3acd85c7412a0ab64df6bb2f9bb14
Alexsander Melo <alexsandergmelo337@gmail.com>
2022-09-10 13:57:29 -0300

    update bandeiras

--- Computação Gráfica/Projeto 2508 (Inicio)/main.cpp
+++ Computação Gráfica/Projeto 2508 (Inicio)/main.cpp
@@ -32,7 +32,7 @@ void desenha(void)
     glEnd();
 
     glBegin(GL_QUADS); // frança
-        glColor3ub(0, 0, 255);
+        glColor3ub(0, 38, 84);
         glVertex2i(5, 645);
         glVertex2i(168, 645);
         glVertex2i(168, 330);
@@ -44,7 +44,7 @@ void desenha(void)
         glVertex2i(337, 330);
         glVertex2i(168, 330);
 
-        glColor3ub(255, 0, 0);
+        glColor3ub(206, 17, 38);
         glVertex2i(337, 645);
         glVertex2i(495, 645);
         glVertex2i(495, 330);
@@ -58,7 +58,7 @@ void desenha(void)
         glVertex2i(626, 435);
         glVertex2i(505, 330);
 
-        glColor3ub(0, 150, 0);
+        glColor3ub(0, 122, 61);
         glVertex2i(505, 645);
         glVertex2i(626, 540);
         glVertex2i(995, 540);
@@ -70,7 +70,7 @@ void desenha(void)
         glVertex2i(995, 435);
         glVertex2i(995, 540);
 
-        glColor3ub(200, 0, 0);
+        glColor3ub(206, 17, 38);
         glVertex2i(626, 435);
         glVertex2i(505, 330);
         glVertex2i(995, 330);
@@ -84,13 +84,13 @@ void desenha(void)
         glVertex2i(495, 157);
         glVertex2i(245, 157);
 
-        glColor3ub(0, 0, 255);
+        glColor3ub(17, 69, 126);
         glVertex2i(5, 320);
         glVertex2i(245, 157);
         glVertex2i(5, 5);
         glVertex2i(5, 320);
 
-        glColor3ub(255, 0, 0);
+        glColor3ub(215, 20, 26);
         glVertex2i(245, 157);
         glVertex2i(5, 5);
         glVertex2i(495, 5);
@@ -105,7 +105,7 @@ void desenha(void)
         glVertex2i(505, 5);
 
         for(int y=5;y<320;y+=35){
-            if (y%2!=0) glColor3ub(0, 0, 255);
+            if (y%2!=0) glColor3ub(13, 94, 175);
             else glColor3ub(255, 255, 255);
             glVertex2i(505, y+35);
             glVertex2i(995, y+35);
@@ -113,7 +113,7 @@ void desenha(void)
             glVertex2i(505, y);
         }
 
-        glColor3ub(0, 0, 255);
+        glColor3ub(13, 94, 175);
         glVertex2i(505, 320);
         glVertex2i(669, 320);
         glVertex2i(669, 145);
@@ -133,14 +133,14 @@ void desenha(void)
     glEnd();
 
     glBegin(GL_QUADS); // eua
-        glColor3ub(255, 0, 0);
+        glColor3ub(180, 4, 9);
         glVertex2i(1005, 330);
         glVertex2i(1495, 330);
         glVertex2i(1495, 645);
         glVertex2i(1005, 645);
         int x = 0;
         for(float y=330; y<620; y+=315/13){
-            if(x%2==0) glColor3ub(255, 0, 0);
+            if(x%2==0) glColor3ub(180, 4, 9);
             else glColor3ub(255, 255, 255);
             x++;
             glVertex2f(1005, y);
@@ -149,13 +149,11 @@ void desenha(void)
             glVertex2f(1005, y+315/13);
         }
 
-        glColor3ub(0, 0, 255);
+        glColor3ub(28, 20, 100);
         glVertex2i(1005, 645);
         glVertex2i(1201, 645);
         glVertex2i(1201, 474);
         glVertex2i(1005, 474);
-
-        glColor3ub(0, 0, 0);
     glEnd();
 
     glColor3ub(255, 255, 255);
@@ -164,10 +162,10 @@ void desenha(void)
             glBegin(GL_POLYGON);
                 glVertex2f(x+13.0, y-15.0);
                 glVertex2f(x+14.0, y-6);
-                glVertex2f(x+04.2, y-26);
-                glVertex2f(x+25.0, y-15.2);
+                glVertex2f(x+04.2, y-24);
+                glVertex2f(x+23.0, y-15.2);
                 glVertex2f(x-1.00, y-15.2);
-                glVertex2f(x+19.8, y-26);
+                glVertex2f(x+19.8, y-24);
             glEnd();
         }
     }
@@ -185,37 +183,54 @@ void desenha(void)
         }
     }
 
-    glBegin(GL_QUADS); // africa do sul
+    glBegin(GL_QUADS); //africa do sul
         glColor3ub(255, 255, 255);
-        glVertex2i(1005, 5);
-        glVertex2i(1495, 5);
         glVertex2i(1495, 320);
+        glVertex2i(1495, 5);
+        glVertex2i(1005, 5);
         glVertex2i(1005, 320);
 
-        glColor3ub(255, 0, 0);
-        glVertex2f(1495, 320);
-        glVertex2f(1495, 209.75);
-        glVertex2f(1266, 209.75);
-        glVertex2f(1156.9, 320);
+        glColor3ub(222, 56, 49);
+        glVertex2i(1495,320);
+        glVertex2i(1495,215);
+        glVertex2i(1266,215);
+        glVertex2i(1105,320);
 
-        glColor3ub(0, 0, 255);
-        glVertex2f(1495, 5);
-        glVertex2f(1495, 115.25);
-        glVertex2f(1266, 115.25);
-        glVertex2f(1156.9, 5);
+        glColor3ub(0, 35 , 149);
+        glVertex2i(1495,5);
+        glVertex2i(1495,110);
+        glVertex2i(1266,110);
+        glVertex2i(1105,5);
+
+        glColor3ub(0, 122, 77);
+        glVertex2f(1259, 193);
+        glVertex2f(1259, 131);
+        glVertex2f(1495, 131);
+        glVertex2f(1495, 193);
     glEnd();
 
     glBegin(GL_POLYGON);
-        glColor3ub(0, 255, 0);
-        glVertex2f(1005, 150);
-        glVertex2f(1005, 320);
-        glVertex2f(1065, 320);
-        glVertex2f(1260, 195);
-        glVertex2f(1495, 195);
-        glVertex2f(1495, 120);
-        glVertex2f(1260, 120);
+        glColor3ub(0, 122, 82);
         glVertex2f(1065, 5);
         glVertex2f(1005, 5);
+        glVertex2f(1005, 43);
+        glVertex2f(1005, 282);
+        glVertex2f(1005, 320);
+        glVertex2f(1065, 320);
+        glVertex2f(1259, 193);
+        glVertex2f(1259, 131);
+    glEnd();
+
+    glBegin(GL_TRIANGLES);
+        glColor3ub(255, 182, 18);
+        glVertex2f(1005, 43);
+        glVertex2f(1005, 281);
+        glVertex2f(1190, 162);
+
+        glColor3ub(0, 0, 0);
+        glVertex2f(1005, 68);
+        glVertex2f(1005, 256);
+        glVertex2f(1151, 162);
     glEnd();
 
 
@@ -237,7 +252,8 @@ int main(void)
     //Tipo de janela (single-buffered) e cores utilizados
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	//Configura o tamanho da janela
-	glutInitWindowSize (1350, 650);
+	//glutInitWindowSize (1350, 650);
+	glutInitWindowSize (999, 444);
 	//Configura a posição inicial da janela
 	glutInitWindowPosition (0, 50);
 
